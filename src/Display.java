4a0243b868afcc274fd7948ba591e19746cf1788
comptergeeks <comptergeeks@gmail.com>
2023-09-05 23:18:49 -0700

    continued work on pixel display and DXYN function

--- src/Display.java
+++ src/Display.java
@@ -5,25 +5,28 @@ public class Display extends JPanel {
     int height;
     int width;
     int scale;
-    Rectangle[][] board;
+    Rectangle[][] pixels;
     public Display(int scale) {
         this.scale = scale;
         width = 64 * scale;
         height = 32 * scale;
-        board = new Rectangle[64][32]; //height (rows), width (columns)
+        pixels = new Rectangle[64][32]; //height (rows), width (columns)
         /*
         logic: turn off and on pixels depending on the current value provided by
         DXYN, make the pixels rectangles depending on the scale and then turn them
         off through that
          */
         setBackground(Color.BLACK);
-        setSize(new Dimension(width, height));
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = new Rectangle(i * scale, j * scale, scale, scale);
-                System.out.println(board[i][j]);
+        //setSize(new Dimension(width, height));
+        getPreferredSize();
+        System.out.println(getSize());
+        for (int i = 0; i < pixels.length; i++) {
+            for (int j = 0; j < pixels[i].length; j++) {
+                pixels[i][j] = new Rectangle(i * scale, j * scale, scale, scale);
+                //System.out.println(board[i][j]);
             }
         }
+        System.out.println(pixels[0].length);
 
     }
     public void draw(CPU cpu, short i, short[] v, int x, int y, int n) {
@@ -34,21 +37,33 @@ public class Display extends JPanel {
         v[0xf] = 0;
         for(int m = 0; m < n; m++) {
            short rowData = cpu.memoryArr[i + m];
-           //for (int c = rawData;  )
+           for (int c = rowData; c< 8; c++) {
+               if (pixels[m][c].)
+           }
 
         }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
+
         super.paintComponent(g);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                Rectangle rect = board[i][j];
+
+        for (int i = 0; i < pixels.length; i++) {
+            for (int j = 0; j < pixels[i].length; j++) {
+                Rectangle rect = pixels[i][j];
                 g.setColor(Color.blue);
                 g.drawRect(rect.x, rect.y, rect.width, rect.height);
+
+                //g.drawString(i + "," + j + " ", rect.x + 4, rect.y - 4);
+                //g.setFont(new Font("TimesRoman", Font.PLAIN, 7));
                 //System.out.println(rect);
             }
         }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return (new Dimension(width, height));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         int scale = 8;
         Display dis = new Display(scale);
         SwingUtilities.invokeLater(() -> {
-
             JFrame displayFrame = new JFrame();
-
-            displayFrame.setSize(64 * scale, 32 * scale);
+            //displayFrame.setSize(64 * scale, 32 * scale);
             displayFrame.getContentPane().add(dis);
             displayFrame.setVisible(true);
             displayFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            displayFrame.setResizable(false);
+            displayFrame.pack();
             /*
                     try {
                         CPU cpu = new CPU(dis);
